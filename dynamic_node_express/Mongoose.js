e9a21c8a09f504f4abe91f216870f24ddd9022ec
Anmol <ag735213@gmail.com>
2023-05-21 17:42:20 +0530

    Second commit

---
+++ dynamic_node_express/Mongoose.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const util = require('util');
+const debug = require('debug')('express-mongoose-es6-rest-api:index');
+const config = require('../config/config');
+const mongoUri = config.mongo.uri;
+mongoose.connect(mongoUri, { keepAlive: 1, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('open', () => {
+    console.log(`connected to the database: ${mongoUri}`);
+});
+
+
+
+if (config.mongo.isDebug) {
+    mongoose.set('debug', (collectionName, method, query, doc) => {
+        debug(`${collectionName}.${method}`, util.inspect(query, false, 20), doc);
+    });
+}
+module.exports = db;
